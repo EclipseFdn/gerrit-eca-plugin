2f48b1733c4480e8e63a366ba91dddd4a97cb5d6
Wayne Beaton <wayne@eclipse.org>
2013-04-25 15:36:02 -0400

    Implemented basic validation flow.
    The various checks are initiated, but not all are actually implemented.
    I've also made a rudimentary first pass at providing reasonable error
    messages. These will be tweaked.

--- eclipse-cla/src/main/java/org/eclipse/foundation/gerrit/validation/EclipseCommitValidationListener.java
+++ eclipse-cla/src/main/java/org/eclipse/foundation/gerrit/validation/EclipseCommitValidationListener.java
@@ -10,13 +10,24 @@
 *******************************************************************************/
 package org.eclipse.foundation.gerrit.validation;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.revwalk.RevCommit;
+
 import com.google.gerrit.extensions.annotations.Listen;
+import com.google.gerrit.reviewdb.client.Account.Id;
+import com.google.gerrit.reviewdb.client.AccountExternalId;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountException;
+import com.google.gerrit.server.account.AccountManager;
 import com.google.gerrit.server.events.CommitReceivedEvent;
 import com.google.gerrit.server.git.validators.CommitValidationException;
 import com.google.gerrit.server.git.validators.CommitValidationListener;
 import com.google.gerrit.server.git.validators.CommitValidationMessage;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /**
@@ -62,11 +73,109 @@ import com.google.inject.Singleton;
 @Listen
 @Singleton
 public class EclipseCommitValidationListener implements CommitValidationListener {
+	@Inject
+	AccountManager accountManager;
+	@Inject
+	IdentifiedUser.GenericFactory factory;
+	
+	/**
+	 * Validate a single commit (this listener will be invoked for each commit in a
+	 * push operation).
+	 */
 	@Override
 	public List<CommitValidationMessage> onCommitReceived(CommitReceivedEvent receiveEvent) 
 			throws CommitValidationException {
+
+		IdentifiedUser user = receiveEvent.user;
+		Project project = receiveEvent.project;
+		
+		RevCommit commit = receiveEvent.commit;
+		PersonIdent authorIdent = commit.getAuthorIdent();
+		
+		List<CommitValidationMessage> messages = new ArrayList<CommitValidationMessage>();
+		messages.add(new CommitValidationMessage(String.format("Reviewing commit: %1$s", commit.getId()), false));
+		messages.add(new CommitValidationMessage(String.format("Authored by: %1$s", authorIdent.getName()), false));
+		
+		/*
+		 * The user must have a presence in Gerrit.
+		 */
+		IdentifiedUser author = identifyUser(authorIdent);
+		if (author == null) {
+			messages.add(new CommitValidationMessage("The author does not have a Gerrit account.", true));
+			messages.add(new CommitValidationMessage("All authors must either be a commiter on the project, or have a current CLA on file.", false));
+			messages.add(new CommitValidationMessage("Please see http://wiki.eclipse.org/CLA", false));
+			throw new CommitValidationException("The author must register with Gerrit.", messages);
+		}		
+		
+		/*
+		 * A committer can author for their own project. Anybody else
+		 * needs to have a current CLA on file.
+		 */
+		if (isCommitter(author, project)) {
+			messages.add(new CommitValidationMessage("The author is a committer on the project.", false));			
+		} else if (!hasCurrentAgreement(author)) {
+			messages.add(new CommitValidationMessage("The author does not have a current Contributor License Agreement (CLA) on file.", true));	
+			messages.add(new CommitValidationMessage("Open your user settings in Gerrit and select \"Agreements\" to create a CLA.", false));	
+			messages.add(new CommitValidationMessage("Please see http://wiki.eclipse.org/CLA", false));
+			throw new CommitValidationException("A Contributor License Agreement is required.", messages);			
+		}
+		
+		/*
+		 * Only committers can push on behalf of other users. Note that, I am asking
+		 * if the user (i.e. the person who is doing the actual push) is a committer.
+		 */
+		if (!author.getAccount().equals(user.getAccount())) {
+			if (!isCommitter(user, project)) {
+				messages.add(new CommitValidationMessage("You are not a project committer.", true));
+				messages.add(new CommitValidationMessage("Only project committers can push on behalf of others.", true));
+				messages.add(new CommitValidationMessage("Please see http://wiki.eclipse.org/CLA", false));
+				throw new CommitValidationException("You must be a committer to push on behalf of others.", messages);
+				
+			}
+		} 
+		
+		// TODO consider adding a control to toggle debug mode.
 		// Just reject it, because that's how we roll...
-		throw new CommitValidationException("I'm sorry " + receiveEvent.user.getName() + ", I can't do that.");
+		// While debugging, it's a bit of a PITA to have to keep creating new commits.
+		throw new CommitValidationException("I'm sorry " + receiveEvent.user.getName() + ", I can't do that.", messages);
 	}
 
+	/**
+	 * <p>Answers whether or not a user has a current committer agreement on file.
+	 * This determination is made based on group membership. Answers <code>true</code>
+	 * if the user is a member of the designated &quot;CLA&quot; group, or
+	 * <code>false</code> otherwise.
+	 * 
+	 * @param user
+	 * @return
+	 */
+	private boolean hasCurrentAgreement(IdentifiedUser user) {
+		// TODO Make the group identity a configurable setting.
+		
+		return false;
+	}
+
+	private boolean isCommitter(IdentifiedUser user, Project project) {
+		return false;
+	}
+
+	/**
+	 * Answers the Gerrit identity (instance of IdentifiedUser) associated with
+	 * the author credentials, or <code>null</code> if the user cannot be
+	 * matched to a Gerrit user identity.
+	 * 
+	 * @param author
+	 *            Object representation of user credentials of a Git commit.
+	 * @return an instance of IdentifiedUser or <code>null</code> if the user
+	 *         cannot be identified by Gerrit.
+	 */
+	private IdentifiedUser identifyUser(PersonIdent author) {
+		try {
+			Id id = accountManager.lookup(AccountExternalId.SCHEME_MAILTO + author.getEmailAddress());
+			if (id == null) return null;
+			return factory.create(id);
+		} catch (AccountException e) {
+			return null;
+		}
+	}
 }
